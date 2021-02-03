0502d8621a36b9e95222b26efd5bc15972fd32c9
Aurelia6 <35572331+Aurelia6@users.noreply.github.com>
2021-02-02 19:00:40 -0500

    Add files via upload

---
+++ Lab 5/drivers/asm/ISRs.s
@@ -0,0 +1,79 @@
+	.text
+
+	.global A9_PRIV_TIM_ISR
+	.global HPS_GPIO1_ISR
+	.global HPS_TIM0_ISR
+	.global HPS_TIM1_ISR
+	.global HPS_TIM2_ISR
+	.global HPS_TIM3_ISR
+	.global FPGA_INTERVAL_TIM_ISR
+	.global FPGA_PB_KEYS_ISR
+	.global FPGA_Audio_ISR
+	.global FPGA_PS2_ISR
+	.global FPGA_JTAG_ISR
+	.global FPGA_IrDA_ISR
+	.global FPGA_JP1_ISR
+	.global FPGA_JP2_ISR
+	.global FPGA_PS2_DUAL_ISR
+	// added to set the flag value
+	.global hps_tim0_int_flag
+
+A9_PRIV_TIM_ISR:
+	BX LR
+	
+HPS_GPIO1_ISR:
+	BX LR
+	
+HPS_TIM0_ISR:
+	PUSH {LR}					// push LR into the stack
+	
+	MOV R0, #0x1
+	BL HPS_TIM_clear_INT_ASM	// clear TIM0
+
+	LDR R0, =hps_tim0_int_flag
+	MOV R1, #1
+	STR R1, [R0]				// set flag to 1
+
+	POP {LR}					// pop LR from the stack
+	BX LR
+	
+HPS_TIM1_ISR:
+	BX LR
+	
+HPS_TIM2_ISR:
+	BX LR
+	
+HPS_TIM3_ISR:
+	BX LR
+	
+FPGA_INTERVAL_TIM_ISR:
+	BX LR
+	
+FPGA_PB_KEYS_ISR:
+	BX LR
+	
+FPGA_Audio_ISR:
+	BX LR
+	
+FPGA_PS2_ISR:
+	BX LR
+	
+FPGA_JTAG_ISR:
+	BX LR
+	
+FPGA_IrDA_ISR:
+	BX LR
+	
+FPGA_JP1_ISR:
+	BX LR
+	
+FPGA_JP2_ISR:
+	BX LR
+	
+FPGA_PS2_DUAL_ISR:
+	BX LR
+
+hps_tim0_int_flag:
+	.word 0x0
+
+	.end
